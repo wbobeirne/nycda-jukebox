12be74a24668bd548ad334e1c433be9b41644ac6
Will O'Beirne <wbobeirne@gmail.com>
2017-04-09 10:41:21 -0400

    Initial file uploading.

--- js/script.js
+++ js/script.js
@@ -12,17 +12,24 @@ var Jukebox = {
 
 	// Run this function to kick the whole thing off
 	start: function() {
+		// Grab the dom elements
 		this.dom = {
 			play: $(".jukebox-controls-play"),
 			stop: $(".jukebox-controls-stop"),
 			next: $(".jukebox-controls-next"),
 			mute: $(".jukebox-controls-mute"),
+			upload: $(".jukebox-header-upload"),
 			songs: $(".jukebox-songs"),
 		};
 
-		var initialSong = this.addSong("./songs/Zimbabwe.mp3");
+		// Queue up some default songs
+		var initialSong = this.addSong("./songs/Zimbabwe.mp3", {
+			title: "Can I Get Wit' Ya in Zimbabwe",
+			artist: "Notorious B.I.G. / New Navy",
+		});
 		this.change(initialSong);
 
+		// Render and bind!
 		this.render();
 		this.listen();
 	},
@@ -34,6 +41,18 @@ var Jukebox = {
 
 		this.dom.mute.on("click", function() {
 			this.setVolume(0);
+		}.bind(this));
+
+		this.dom.upload.on("change", function() {
+			var files = this.dom.upload.prop("files");
+
+			for (var i = 0; i < files.length; i++) {
+				var file = URL.createObjectURL(files[i]);
+				this.addSong(file, {
+					title: "Uploaded song",
+					artist: "Unknown",
+				});
+			}
 		}.bind(this));
 
 		this.dom.stop.on("click", this.stop.bind(this));
@@ -69,11 +88,25 @@ var Jukebox = {
 	},
 
 	pause: function() {
-		console.log("Jukebox is pauseing");
+		if (!this.activeSong) {
+			return false;
+		}
+
+		this.activeSong.pause();
+		this.isPlaying = false;
+		this.render();
+		return this.activeSong;
 	},
 
 	stop: function() {
-		console.log("Jukebox is stoping");
+		if (!this.activeSong) {
+			return false;
+		}
+
+		this.activeSong.stop();
+		this.isPlaying = false;
+		this.render();
+		return this.activeSong;
 	},
 
 	change: function(song) {
@@ -92,9 +125,10 @@ var Jukebox = {
 		console.log("Jukebox is shuffleing");
 	},
 
-	addSong: function(path) {
-		var song = new Song(path);
+	addSong: function(file, meta) {
+		var song = new Song(file, meta);
 		this.songs.push(song);
+		this.render();
 		return song;
 	},
 
@@ -110,13 +144,23 @@ var Jukebox = {
  * SONG CLASS
  */
 class Song {
-	constructor(file) {
+	constructor(file, meta) {
 		this.file = file;
+		this.meta = meta || {
+			title: "Unknown title",
+			artist: "Unknown artist",
+		};
 		this.audio = new Audio(file);
 	}
 
 	render() {
-		return $('<div class="jukebox-songs-song">' + this.file + '</div>');
+		var $song = $('<div class="jukebox-songs-song"></div>');
+		$song.append('<div class="jukebox-songs-song-pic"></div>');
+		$song.append('<div class="jukebox-songs-song-title">' + this.meta.title + '</div>');
+		$song.append('<div class="jukebox-songs-song-artist">' + this.meta.artist + '</div>');
+		$song.append('<div class="jukebox-songs-song-duration">' + "3:22" + '</div>');
+		$song.data("song", this);
+		return $song;
 	}
 
 	play() {
@@ -124,11 +168,12 @@ class Song {
 	}
 
 	pause() {
-
+		this.audio.pause();
 	}
 
 	stop() {
-
+		this.audio.pause();
+		this.audio.currentTime = 0;
 	}
 }
 
